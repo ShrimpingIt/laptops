c370ccaefe1e52ee116d0e361bf196ea0bb81b28
Cefn Hoile <github.com@cefn.com>
2016-06-14 19:33:23 +0100

    Fallback on fixed IP of Laptop 1 ad DHCP host

--- scripts/sync_apt.sh
+++ scripts/sync_apt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PASSWORD=$1
-for SUFFIX in $(seq 2 9)
+for SUFFIX in `./numbers.sh`
 do
 	echo -n "${SUFFIX}: "
-	ssh -t laptop@debian-shrimp-"$SUFFIX" "echo '${PASSWORD}' | sudo -S rsync -e 'ssh -i /home/laptop/.ssh/id_rsa' --verbose --whole-file --archive --compress --recursive laptop@debian-shrimp-1:/var/cache/apt/archives/ /var/cache/apt/archives/"
+	ssh -t laptop@debian-shrimp-"$SUFFIX" "echo '${PASSWORD}' | sudo -S rsync -e 'ssh -i /home/laptop/.ssh/id_rsa' --verbose --whole-file --archive --compress --recursive --include='chromium*' --include='iceweasel*' laptop@10.42.0.1:/var/cache/apt/archives/ /var/cache/apt/archives/"
 done
