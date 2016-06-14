d63fbee9ec86af0941f6d59404b505c883f4a93b
Cefn Hoile <github.com@cefn.com>
2016-06-14 19:31:26 +0100

    Now targeting specific version of Arduino

--- scripts/resetarduino.sh
+++ scripts/resetarduino.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-for SUFFIX in 0 $(seq 2 9)
+for SUFFIX in `./numbers.sh`
 do
 	echo -n "${SUFFIX}: "
 	# reset sketchbook location
-	ssh -t laptop@debian-shrimp-"$SUFFIX" "rsync -e 'ssh -i /home/laptop/.ssh/id_rsa' --verbose --whole-file --archive --compress --recursive laptop@debian-shrimp-1:/home/laptop/sketchbook/ /home/laptop/sketchbook/"
+	#ssh -t laptop@debian-shrimp-"$SUFFIX" "rsync -e 'ssh -i /home/laptop/.ssh/id_rsa' --verbose --whole-file --archive --compress --recursive laptop@debian-shrimp-1:/home/laptop/sketchbook/ /home/laptop/sketchbook/"
 	# shutdown running arduino instances
 	ssh laptop@debian-shrimp-"$SUFFIX" "killall java"
 	# launch a new version of arduino
-	ssh laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 nohup arduino /home/laptop/sketchbook/shrimPov/shrimPov.ino  > /dev/null &"
+	ssh laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 nohup ~/Desktop/arduino-1.6.3/arduino > /dev/null &"
 	
+	#/home/laptop/sketchbook/shrimPov/shrimPov.ino
 	#/usr/share/arduino/examples/01.Basics/Blink/Blink.ino
 done
