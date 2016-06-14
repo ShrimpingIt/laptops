cad13b1a6fc50614edd71c49066ae9d372bac4b7
Cefn Hoile <github.com@cefn.com>
2016-06-14 19:32:05 +0100

    Augmented to send fullscreen keys for extra control

--- scripts/runchrome.sh
+++ scripts/runchrome.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-for SUFFIX in $(seq 0 9)
+#URL="http://lovedbyparents.com/wp-content/uploads/2012/12/mike-7.jpg"
+URL="http://i1.mirror.co.uk/incoming/article232023.ece/alternates/s2197/postman-pat-pic-bbc-image-1-719090620.jpg"
+#URL="http://shrimping.it"
+#APP=chromium-browser
+APP=iceweasel
+TOKILL=postman
+TOFULLSCREEN=postman
+for SUFFIX in `./numbers.sh`
 do
 	echo -n "${SUFFIX}: "
-	ssh laptop@debian-shrimp-"$SUFFIX" "killall /opt/google/chrome/chrome"
-	ssh laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 xdg-open http://shrimping.it > /dev/null &"
+	ssh laptop@debian-shrimp-"$SUFFIX" "killall iceweasel" & 
+# suffers from race condition - sometimes iceweasel, sometimes chromium
+#	ssh -t -t laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 xdg-open ${URL} > /dev/null &" & 
+#	ssh -t -t laptop@debian-shrimp-"$SUFFIX" "${APP} ${URL} > /dev/null &" &
 done
 
-for SUFFIX in $(seq 0 9)
-do
-	ssh laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 xdotool search chrome windowactivate --sync key F11"
-done
+#for SUFFIX in `./numbers.sh`
+#do
+#	ssh laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 nohup $APP $URL > /dev/null" &
+#done
+
+# SEND FULLSCREEN KEYS
+#for SUFFIX in `./numbers.sh`
+#do
+#	ssh laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 xdotool search ${TOFULLSCREEN} windowactivate --sync key F11" &
+#done
