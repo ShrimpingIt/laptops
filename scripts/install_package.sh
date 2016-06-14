409e455d0d0cd83aaa4ab67edc71fc4c9f01d118
Cefn Hoile <github.com@cefn.com>
2016-06-14 19:29:32 +0100

    Used ./numbers.sh different packages being installed

--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 PASSWORD=$1
-for SUFFIX in $(seq 0 9)
+for SUFFIX in 1
 do
 	echo -n "${SUFFIX}: "
-	ssh -t laptop@debian-shrimp-"$SUFFIX" "echo '${PASSWORD}' | sudo -S apt-get -y install ntpdate"
+	ssh -t laptop@debian-shrimp-"$SUFFIX" "echo '${PASSWORD}' | sudo -S apt-get update"
+	ssh -t laptop@debian-shrimp-"$SUFFIX" "echo '${PASSWORD}' | sudo -S apt-get -y install chromium-browser iceweasel"
 done
 #arduino-mk
