13df394d2977a2257218c6cc4cec178394043130
Cefn Hoile <github.com@cefn.com>
2016-10-02 20:09:26 +0100

    Precede reports with laptop number

--- scripts/memreport.sh
+++ scripts/memreport.sh
@@ -2,9 +2,8 @@
 PASSWORD=$1
 for SUFFIX in `./numbers.sh`
 do
-	echo -n "${SUFFIX}: "
 	# reports on modules
 	#ssh -t laptop@debian-shrimp-"$SUFFIX" "echo '${PASSWORD}' | sudo -S echo; echo -n 'Modules:'; sudo dmidecode --type memory | grep Size;"
-	ssh -t -q laptop@debian-shrimp-"$SUFFIX" "echo '${PASSWORD}' | sudo -p '' -S echo 2>&1 1>/dev/null; echo -n 'Memory:'; sudo free | awk '/Mem/ { print \$2;}' "
+	ssh -t -q laptop@debian-shrimp-"$SUFFIX" "echo -n ${SUFFIX}: ; echo '${PASSWORD}' | sudo -p '' -S echo 2>&1 1>/dev/null; echo -n 'Memory:'; sudo free | awk '/Mem/ { print \$2;}' "
 done
 #arduino-mk
