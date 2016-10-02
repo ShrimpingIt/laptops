bca94317788e690f57f3ca8cb73d99e866e777f7
Cefn Hoile <github.com@cefn.com>
2016-10-02 20:10:22 +0100

    Made SKETCH an env var.

--- scripts/resetarduino.sh
+++ scripts/resetarduino.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 for SUFFIX in `./numbers.sh`
 do
+	#export SKETCH="/home/laptop/sketchbook/shrimPov/shrimPov.ino"
+	# export SKETCH="/usr/share/arduino/examples/01.Basics/Blink/Blink.ino"
+	export SKETCH=""
 	echo -n "${SUFFIX}: "
-	# reset sketchbook location
-	#ssh -t laptop@debian-shrimp-"$SUFFIX" "rsync -e 'ssh -i /home/laptop/.ssh/id_rsa' --verbose --whole-file --archive --compress --recursive laptop@debian-shrimp-1:/home/laptop/sketchbook/ /home/laptop/sketchbook/"
 	# shutdown running arduino instances
-	ssh laptop@debian-shrimp-"$SUFFIX" "killall java"
+	ssh laptop@debian-shrimp-"$SUFFIX" "killall java;" 
 	# launch a new version of arduino
-	ssh laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 nohup ~/Desktop/arduino-1.6.3/arduino > /dev/null &"
-	
-	#/home/laptop/sketchbook/shrimPov/shrimPov.ino
-	#/usr/share/arduino/examples/01.Basics/Blink/Blink.ino
+	ssh laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 nohup ~/Desktop/arduino-1.6.3/arduino ${SKETCH} > /dev/null &" & 
 done
