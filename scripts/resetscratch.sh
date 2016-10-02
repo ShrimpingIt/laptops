ec42fd75e300d3c18438f91c7094b0d8c5d40dfe
Cefn Hoile <github.com@cefn.com>
2016-10-02 20:14:51 +0100

    New files

---
+++ scripts/resetscratch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for SUFFIX in `./numbers.sh`
+do
+	#export SKETCH="/home/laptop/sketchbook/shrimPov/shrimPov.ino"
+	# export SKETCH="/usr/share/arduino/examples/01.Basics/Blink/Blink.ino"
+	export SKETCH=""
+	echo -n "${SUFFIX}: "
+	# shutdown running arduino instances
+	ssh laptop@debian-shrimp-"$SUFFIX" "killall squeakvm;" 
+	# launch scratch piano
+	ssh laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 nohup xdg-open ~/Desktop/projects/utilities/keyboard/shrimpkey_piano.sb > /dev/null &" & 	
+done
