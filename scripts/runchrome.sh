2b7c1e409b17548f64500df0de40354fe3ac8ce8
Cefn Hoile <github.com@cefn.com>
2016-10-02 20:11:19 +0100

    Made app explicit instead of xdg-open

--- scripts/runchrome.sh
+++ scripts/runchrome.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #URL="http://lovedbyparents.com/wp-content/uploads/2012/12/mike-7.jpg"
-URL="http://i1.mirror.co.uk/incoming/article232023.ece/alternates/s2197/postman-pat-pic-bbc-image-1-719090620.jpg"
-#URL="http://shrimping.it"
-#APP=chromium-browser
-APP=iceweasel
+#URL="http://i1.mirror.co.uk/incoming/article232023.ece/alternates/s2197/postman-pat-pic-bbc-image-1-719090620.jpg"
+URL="http://start.shrimping.it/project/keyboard/build.html"
+APP=chromium-browser
+#APP=iceweasel
 TOKILL=postman
 TOFULLSCREEN=postman
 for SUFFIX in `./numbers.sh`
 do
 	echo -n "${SUFFIX}: "
-	ssh laptop@debian-shrimp-"$SUFFIX" "killall iceweasel" & 
-# suffers from race condition - sometimes iceweasel, sometimes chromium
-#	ssh -t -t laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 xdg-open ${URL} > /dev/null &" & 
-#	ssh -t -t laptop@debian-shrimp-"$SUFFIX" "${APP} ${URL} > /dev/null &" &
+	ssh laptop@debian-shrimp-"$SUFFIX" "killall ${APP}" & 
+	# suffers from race condition - sometimes iceweasel, sometimes chromium
+	ssh -t -t laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 chromium-browser ${URL} > /dev/null &" & 
+#	ssh -t -t laptop@debian-shrimp-"$SUFFIX" "DISPLAY=:0.0 nohup ${APP} ${URL} > /dev/null &" &
 done
 
 #for SUFFIX in `./numbers.sh`
