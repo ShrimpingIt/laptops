c1f902a1ddd7e09fb2c362bc8c587438cb72de6f
Cefn Hoile <github.com@cefn.com>
2016-10-02 20:08:46 +0100

    Modified backup script for new folder heirarchy

--- scripts/backup_desktops.sh
+++ scripts/backup_desktops.sh
@@ -2,7 +2,7 @@
 for SUFFIX in `./numbers.sh`
 do
 	echo -n "${SUFFIX}: "
-	LOCALDIR=../combined-desktop/
+	LOCALDIR=../../combined-desktop/
 	mkdir -p $LOCALDIR
-	rsync --recursive --archive --backup --whole-file -e ssh laptop@debian-shrimp-"${SUFFIX}":~/Desktop $LOCALDIR/
+	rsync --recursive --archive --backup --whole-file --verbose --progress -e ssh laptop@debian-shrimp-"${SUFFIX}":~/Desktop $LOCALDIR/
 done
