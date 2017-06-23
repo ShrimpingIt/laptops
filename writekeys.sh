50029005b19c0d403b9161d81ddc278ee5673e92
@ShrimpingIt Studio Laptop account <shrimpingit_studio_laptop@cefn.com>
2017-06-23 22:09:10 +0100

    Script for flashing the USB keys from a master in /dev/sdb

---
+++ writekeys.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo dd if=/dev/sdb of=/dev/sdc bs=128k; echo "Wrote sdc"
+sudo dd if=/dev/sdb of=/dev/sdd bs=128k; echo "Wrote sdd"
+sudo dd if=/dev/sdb of=/dev/sde bs=128k; echo "Wrote sde"
+sudo dd if=/dev/sdb of=/dev/sdf bs=128k; echo "Wrote sdf"
+sudo dd if=/dev/sdb of=/dev/sdg bs=128k; echo "Wrote sdg"
+sudo dd if=/dev/sdb of=/dev/sdh bs=128k; echo "Wrote sdh"
+sudo dd if=/dev/sdb of=/dev/sdi bs=128k; echo "Wrote sdi"
