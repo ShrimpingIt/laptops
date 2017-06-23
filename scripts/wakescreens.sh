e11f54a40f7ef72c7a3bd1d039828bb1d60936a5
@ShrimpingIt Studio Laptop account <shrimpingit_studio_laptop@cefn.com>
2017-06-23 22:56:44 +0100

    No longer using Gnome on laptops

--- scripts/wakescreens.sh
+++ scripts/wakescreens.sh
@@ -3,5 +3,4 @@ for SUFFIX in `./numbers.sh`
 do
 	echo -n "${SUFFIX}: "
 	ssh -t -t learner@debian-shrimp-"$SUFFIX" "DISPLAY=:0 xset dpms force on" &
-	ssh -t -t learner@debian-shrimp-"$SUFFIX" "DISPLAY=:0 gnome-screensaver-command -d" &
 done
