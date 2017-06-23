22b3077dadb5935bf744a115a41458b8a82cb716
@ShrimpingIt Studio Laptop account <shrimpingit_studio_laptop@cefn.com>
2017-06-23 22:07:12 +0100

    Changed back blocksize to a better number

--- fs/write_partition.sh
+++ fs/write_partition.sh
@@ -2,4 +2,4 @@
 GZIP=gzip
 # pigz if you want to use multiple cores (increasing speed of compression/decompression)
 #GZIP=pigz
-sudo ${GZIP} -dc /media/lubuntu/DATA/snapshot.iso.gz | sudo dd of=/dev/sda bs=1k
+sudo ${GZIP} -dc /media/lubuntu/DATA/snapshot.iso.gz | sudo dd of=/dev/sda bs=128k
