32c650c289c64346a1c9bdedd49a14f7ae97ba03
@ShrimpingIt Studio Laptop account <shrimpingit_studio_laptop@cefn.com>
2017-06-24 01:04:14 +0100

    Attempted to emulate gnome-screensaver-command with equivalent light-locker-command. Now commented as it reported screensaver not disabled

--- scripts/wakescreens.sh
+++ scripts/wakescreens.sh
@@ -3,4 +3,5 @@ for SUFFIX in `./numbers.sh`
 do
 	echo -n "${SUFFIX}: "
 	ssh -t -t learner@debian-shrimp-"$SUFFIX" "DISPLAY=:0 xset dpms force on" &
+	#ssh -t -t learner@debian-shrimp-"$SUFFIX" "DISPLAY=:0 light-locker-command -d" &
 done
